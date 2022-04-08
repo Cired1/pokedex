9287637aca5c04bc673b47a7ed65ec42d433e841
Cired1 <edabundis@gmail.com>
2022-04-07 20:33:13 -0500

    Add UI to home and pokemon route

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,8 +1,20 @@
+import usePokemons from "../../hooks/usePokemons";
+import Container from 'react-bootstrap/Container'
+import List from "../../components/List/List";
+
 const Home = () => {
+
+    const { pokemons, pokemonURLStatus } = usePokemons();
+
     return (
-        <div>
-          home  
-        </div>
+        <Container>
+            {pokemonURLStatus === "loading"
+                ? <>Loading...</>
+                : <List
+                    pokemons={pokemons}
+                />
+            }
+        </Container>
     )
 }
 
--- src/routes/Pokemon/Pokemon.js
+++ src/routes/Pokemon/Pokemon.js
@@ -1,9 +1,17 @@
+import Container from "react-bootstrap/esm/Container"
+import usePokemon from "../../hooks/usePokemon";
 
 const Pokemon = () => {
+
+    const { pokemon, pokemonStatus } = usePokemon();
+
     return (
-        <div>
-            pokemon
-        </div>
+        <Container>
+            {pokemonStatus === "loading"
+                ? <h2>loading...</h2>
+                : pokemon.name
+            }
+        </Container>
     )
 }
 
